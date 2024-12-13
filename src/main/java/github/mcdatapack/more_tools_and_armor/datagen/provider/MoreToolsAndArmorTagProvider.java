c2af421a046aedf5d60336e2e0bfefe3833014d1
MC-datapack (Timo_11) <timo.tewes@web.de>
2024-12-13 21:15:41 +0100

    6.1

--- src/main/java/github/mcdatapack/more_tools_and_armor/datagen/provider/MoreToolsAndArmorTagProvider.java
+++ src/main/java/github/mcdatapack/more_tools_and_armor/datagen/provider/MoreToolsAndArmorTagProvider.java
@@ -195,6 +195,11 @@ public class MoreToolsAndArmorTagProvider{
                     .add(ONETHDENDERITE_LEGGINGS)
                     .add(ONETHDENDERITE_CHESTPLATE)
                     .add(ONETHDENDERITE_HELMET);
+
+            getOrCreateTagBuilder(ItemTags.SHARP_WEAPON_ENCHANTABLE)
+                    .add(ItemInit.WOODEN_PAXEL, ItemInit.STONE_PAXEL, ItemInit.IRON_PAXEL, ItemInit.GOLDEN_PAXEL, ItemInit.DIAMOND_PAXEL,
+                            ItemInit.NETHERITE_PAXEL, ItemInit.COPPER_PAXEL, ItemInit.BUDDING_AMETHYST_PAXEL, ItemInit.EMERALD_PAXEL, ItemInit.OBSIDIAN_PAXEL,
+                            ItemInit.DEEPSLATE_EMERALD_PAXEL, ItemInit.END_DIAMOND_PAXEL, ItemInit.VOID_PAXEL, ItemInit.ONETHDENDERITE_PAXEL);
         }
     }
 }
