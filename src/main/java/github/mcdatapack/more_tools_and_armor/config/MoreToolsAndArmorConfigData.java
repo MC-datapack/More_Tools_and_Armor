69092533168cdf52daa7a704fa1fa08263b4ade7
MC-datapack (Timo_11) <timo.tewes@web.de>
2025-02-22 20:13:32 +0100

    8.2

--- src/main/java/github/mcdatapack/more_tools_and_armor/config/MoreToolsAndArmorConfigData.java
+++ src/main/java/github/mcdatapack/more_tools_and_armor/config/MoreToolsAndArmorConfigData.java
@@ -3,6 +3,7 @@ package github.mcdatapack.more_tools_and_armor.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -13,7 +14,6 @@ import java.util.List;
 import java.util.Objects;
 
 import static github.mcdatapack.more_tools_and_armor.init.ArmorMaterialInit.*;
-import static net.minecraft.item.ArmorMaterials.NETHERITE;
 
 @Config(name = "more_tools_and_armor")
 public class MoreToolsAndArmorConfigData implements ConfigData {
@@ -33,6 +33,7 @@ public class MoreToolsAndArmorConfigData implements ConfigData {
     public AbilityConfig endermanPassive = new AbilityConfig(true, OLED);
 
     @ConfigEntry.Gui.CollapsibleObject
+    @Comment("Instand Damage always gets applied")
     public AbilityConfig statusEffectImmune = new AbilityConfig(true, OLED);
 
     public static class AbilityConfig {
